4d0a13cf4b8351fa13dbb1e24b1c02ead2c493a7
Nikhil Ghadshi <68466113+NikhilGhadshi@users.noreply.github.com>
2021-04-29 12:21:13 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,7 +1,7 @@
 <?php  
      	define('DB_SERVER', 'remotemysql.com');
 	define('DB_USERNAME', 'KS7qDr6d3U');
-	define('DB_PASSWORD', 'x46IbSltx1');
+	define('DB_PASSWORD', 'OoXfmnsQpC');
 	define('DB_NAME', 'KS7qDr6d3U');
 
        	$conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD,DB_NAME);
